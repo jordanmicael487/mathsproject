29d946faa0f83c050011a65b37413846d7a9fec0
jordanmicael487 <jordanmicael487@gmail.com>
2025-04-06 05:25:03 +0000

    Add 25.rs

---
+++ src/25.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Example Rust code to demonstrate basic usage of functions and variables
+    let x = 10;
+    println!("The value of x is: {}", x);
+    
+    let y = "Hello, World!";
+    println!("The value of y is: {}", y);
+}
