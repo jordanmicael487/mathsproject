c3a6b6a545c5664d68cab6137a43718cee3fd2b9
jordanmicael487 <jordanmicael487@gmail.com>
2025-05-26 19:32:24 +0000

    Add 45.rs

---
+++ src/45.rs
@@ -0,0 +1,9 @@
+// This is a simple example of a Rust program.
+fn main() {
+    // Define some variables
+    let x = 10;
+    let y = 5;
+
+    // Perform arithmetic operations on the variables
+    println!("The result of {} + {} is {}", x, y, x.add(y));
+}
